1e47170d2577f42a265021db8c1294f49bcabbf2
Frank Micheal <harlex.mikkey@gmail.com>
2024-03-04 23:57:48 +0100

    adding mobile menu linear gradient bg, toggling the bg on mobile menu open or close

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 const btn = document.getElementById("menu-btn");
 const nav = document.getElementById("menu");
+const mobileBg = document.querySelector(".mobile-bg");
 btn.addEventListener("click", () => {
    btn.classList.toggle("open");
    nav.classList.toggle("flex");
    nav.classList.toggle("hidden");
+   mobileBg.classList.toggle("hidden");
+   mobileBg.classList.toggle("linear-gradient-bg");
 });
