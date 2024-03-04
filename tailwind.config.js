81f3acff6d24e0eac20fcf5262cd567e15c5359e
Frank Micheal <harlex.mikkey@gmail.com>
2024-03-04 19:52:50 +0100

    making changes to the layout, utilizing flex box gap for proper spacing, reconfiguring the screen breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,10 @@ module.exports = {
    content: ["./**/*.{html,js}"],
    theme: {
       screens: {
-         sm: "480px",
+         sm: "576px",
          md: "768px",
-         lg: "976px",
-         xl: "1440px",
+         lg: "1120px",
+         xl: "1240px",
       },
       extend: {
          colors: {
